255ec3df7e4ee46b14d6a8951ae04f2fe30ae52d
Alexander Kozienko <pascalamsg@gmail.com>
2017-06-21 11:24:22 +0300

    quartz configuration test

---
+++ pkg/bridge/Tests/DI/QuartzConfigurationTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace Quartz\Bridge\Tests\DI;
+
+use PHPUnit\Framework\TestCase;
+use Quartz\Bridge\DI\QuartzConfiguration;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Processor;
+
+class QuartzConfigurationTest extends TestCase
+{
+    public function testShouldImplementConfigurationInterface()
+    {
+        $this->assertInstanceOf(ConfigurationInterface::class, new QuartzConfiguration());
+    }
+
+    public function testShouldReturnDefaultConfig()
+    {
+        $configuration = new QuartzConfiguration([]);
+
+        $processor = new Processor();
+        $config = $processor->processConfiguration($configuration, [[]]);
+
+        $expectedConfig = [
+            'store' => [
+                'uri' => 'mongodb://localhost:27017',
+                'uriOptions' => [],
+                'driverOptions' => [],
+                'sessionId' => 'quartz',
+                'dbName' => 'quartz',
+                'managementLockCol' => 'managementLock',
+                'calendarCol' => 'calendar',
+                'triggerCol' => 'trigger',
+                'firedTriggerCol' => 'firedTrigger',
+                'jobCol' => 'job',
+                'pausedTriggerCol' => 'pausedTrigger',
+            ],
+            'misfireThreshold' => 60,
+        ];
+
+        $this->assertSame($expectedConfig, $config);
+    }
+
+    public function testCouldSetConfigurationOptions()
+    {
+        $configuration = new QuartzConfiguration([]);
+
+        $processor = new Processor();
+        $config = $processor->processConfiguration($configuration, [[
+            'store' => [
+                'uri' => 'the-uri',
+            ],
+            'misfireThreshold' => 120,
+        ]]);
+
+        $expectedConfig = [
+            'store' => [
+                'uri' => 'the-uri',
+                'uriOptions' => [],
+                'driverOptions' => [],
+                'sessionId' => 'quartz',
+                'dbName' => 'quartz',
+                'managementLockCol' => 'managementLock',
+                'calendarCol' => 'calendar',
+                'triggerCol' => 'trigger',
+                'firedTriggerCol' => 'firedTrigger',
+                'jobCol' => 'job',
+                'pausedTriggerCol' => 'pausedTrigger',
+            ],
+            'misfireThreshold' => 120,
+        ];
+
+        $this->assertSame($expectedConfig, $config);
+    }
+}
